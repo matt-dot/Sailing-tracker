8e14c96ac9906e8c9df95d4dabc09705f4fc4be4
matt-dot <62967152+matt-dot@users.noreply.github.com>
2020-08-04 12:20:00 +0100

    "created login and register fragments"

--- app/src/main/java/com/example/sailing_tracker/MainActivity.java
+++ app/src/main/java/com/example/sailing_tracker/MainActivity.java
@@ -1,26 +1,59 @@
 package com.example.sailing_tracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Bundle;
+import android.content.res.Resources;
+import android.os.Build;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.sailing_tracker.MESSAGE";
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>;
+        public AuthenticationPagerAdapter (FragmentManager fm) {
+            super(fm);
+
+        }
+        @Override
+        public Fragment getItem(int i){
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+        void addFragment(Fragment fragment){
+            fragmentList.add(fragment);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void sendMessage (View view){
-        Intent intent = new Intent (this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new
+                AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+
     }
 }
