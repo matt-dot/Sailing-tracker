3ff215f16ca4dd73b87a99c4cec469b80acba4ae
matt <matthew.langley3@btinternet.com>
2020-09-18 17:01:19 +0100

    "firebase interaction unsuccessful"

--- app/src/main/java/com/example/sailing_tracker/MainActivity.java
+++ app/src/main/java/com/example/sailing_tracker/MainActivity.java
@@ -1,25 +1,16 @@
 package com.example.sailing_tracker;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.os.Build;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.annotation.RequiresApi;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.jar.Attributes;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText Email;
     private EditText Password;
     private Button LoginButton;
+
 
 
     @Override
@@ -40,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         Password = (EditText) findViewById(R.id.et_password);
         LoginButton = (Button) findViewById(R.id.btn_login);
 
+
+
         LoginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view){
@@ -47,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+
 
 
     }
@@ -58,5 +54,11 @@ public class MainActivity extends AppCompatActivity {
         } else {
             
         }
+
+
+
     }
+
+
+
 }
