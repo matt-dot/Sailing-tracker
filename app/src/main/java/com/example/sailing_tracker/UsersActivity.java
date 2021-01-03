53fc4bd5a84d18a48b9ad0a6bc1f1ecdb9d04228
matt-dot <62967152+matt-dot@users.noreply.github.com>
2021-01-03 19:41:56 +0000

    "added get last location to record button"

--- app/src/main/java/com/example/sailing_tracker/UsersActivity.java
+++ app/src/main/java/com/example/sailing_tracker/UsersActivity.java
@@ -1,10 +1,10 @@
 package com.example.sailing_tracker;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,9 +25,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsersActivity extends AppCompatActivity{
-    Context context;
+
+    // Init of variables
     private List<ModelUser> userList;
-    private AdapterUsers adapterUsers;
     private RecyclerView recyclerView;
 
     FirebaseAuth firebaseAuth;
@@ -41,7 +41,8 @@ public class UsersActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
 
-        // Toolbar
+        // Toolbar init
+        // The toolbar is instantiated and assigned a title
         final Toolbar mToolBar = findViewById(R.id.my_toolbar);
         setSupportActionBar(mToolBar);
         mToolBar.setTitle("Users");
@@ -50,14 +51,13 @@ public class UsersActivity extends AppCompatActivity{
         recyclerView = (RecyclerView) findViewById(R.id.users_recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
+        // Init the array list to storing the list of users
         userList = new ArrayList<>();
-
-
-
 
         // Firebase init
         firebaseAuth = firebaseAuth.getInstance();
+
+        // Method call to populate userList
         getAllUsers();
 
         }
@@ -71,33 +71,24 @@ public class UsersActivity extends AppCompatActivity{
             databaseReference.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    // Clear user list before populating
                     userList.clear();
+                    // Iterate through the firebase database and assign each iteration to ModelUser class
                     for (DataSnapshot ds : snapshot.getChildren()) {
                         ModelUser modelUser = ds.getValue(ModelUser.class);
                         // Get all users except one currently signed in
                         if (!modelUser.getUid().equals(firebaseUser.getUid())) {
                             userList.add(modelUser);
                         }
-
-
                         AdapterUsers mAdapter = new AdapterUsers(userList);
                         recyclerView.setAdapter(mAdapter);
-
-
-
-
-
                     }
-
                 }
-
                 @Override
                 public void onCancelled(@NonNull DatabaseError error) {
-
+                    Toast.makeText(UsersActivity.this, ""+ error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
-
-
         }
 
     private void checkUserStatus() {
@@ -132,8 +123,6 @@ public class UsersActivity extends AppCompatActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
-
         return true;
     }
 
