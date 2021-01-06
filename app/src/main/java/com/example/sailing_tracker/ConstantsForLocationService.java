e49c1f74be006d155e929f8841ec64e58a3eeddb
matt-dot <62967152+matt-dot@users.noreply.github.com>
2021-01-06 20:31:13 +0000

    "progress on foreground LocationService"

---
+++ app/src/main/java/com/example/sailing_tracker/ConstantsForLocationService.java
@@ -0,0 +1,9 @@
+package com.example.sailing_tracker;
+
+class ConstantsForLocationService {
+    static final int LOCATION_SERVICE_ID = 180;
+    static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
+    static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
+
+
+}
--- app/src/main/java/com/example/sailing_tracker/LocationService.java
+++ app/src/main/java/com/example/sailing_tracker/LocationService.java
@@ -3,14 +3,45 @@ package com.example.sailing_tracker;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 
 public class LocationService extends Service {
-    public LocationService() {
-    }
 
+
+
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            super.onLocationResult(locationResult);
+            // If both locationResult and last location are not null then...
+            // This avoids null pointer exceptions as there are instances where last known location
+            // will be null
+            if(locationResult != null && locationResult.getLastLocation() != null){
+                // ...assign the user current location to lat and long variables
+                double latitude = locationResult.getLastLocation().getLatitude();
+                double longitude = locationResult.getLastLocation().getLongitude();
+
+
+                // Output to log
+                Log.d("LOCATION_UPDATE", "Lat: "+ latitude + "," + "Long: " + longitude);
+            }
+
+            }
+        };
+
+
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
+
+
+
