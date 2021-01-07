73b1d9ec97d8e098eafbac9b288766514ddd11f2
matt-dot <62967152+matt-dot@users.noreply.github.com>
2021-01-07 18:55:20 +0000

    "foreground LocationService working, need to bind location data to it"

--- app/src/main/java/com/example/sailing_tracker/LocationService.java
+++ app/src/main/java/com/example/sailing_tracker/LocationService.java
@@ -1,21 +1,27 @@
 package com.example.sailing_tracker;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationService extends Service {
 
-
-
-
-    private LocationCallback locationCallback = new LocationCallback() {
+    private final LocationCallback locationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(LocationResult locationResult) {
             super.onLocationResult(locationResult);
@@ -41,7 +47,91 @@ public class LocationService extends Service {
     public IBinder onBind(Intent intent) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+
+    @SuppressLint("MissingPermission")
+    private void startLocationService(){
+        // Set the id of the channel
+        String channelId = "location_notification_channel";
+        // Init NotificationManager
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent resultIntent = new Intent();
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                0,
+                resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        Context context;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                getApplicationContext(),
+                channelId
+        );
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentTitle("Location Service");
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        builder.setContentText("Running");
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(false);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            if (notificationManager != null && notificationManager.getNotificationChannel(channelId) == null){
+                CharSequence name;
+                NotificationChannel notificationChannel = new NotificationChannel(
+                        channelId,
+                        "LocationService",
+                        NotificationManager.IMPORTANCE_HIGH);
+                notificationChannel.setDescription("This channel is used by location service");
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(5000); // Store me in a constant
+            locationRequest.setFastestInterval(2000); // Store me in a constant
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+
+            LocationServices.getFusedLocationProviderClient(this)
+                    .requestLocationUpdates(locationRequest, locationCallback, null); // May cause error
+
+            startForeground(ConstantsForLocationService.LOCATION_SERVICE_ID, builder.build());
+
+
+            }
+
+
+        }
+
+
+
+
+
+    private void stopLocationService(){
+        LocationServices.getFusedLocationProviderClient(this)
+                .removeLocationUpdates(locationCallback);
+        stopForeground(true);
+        stopSelf();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null){
+             String action = intent.getAction();
+             if (action != null){
+                 if(action.equals(ConstantsForLocationService.ACTION_START_LOCATION_SERVICE)){
+                     startLocationService();
+                 } else if (action.equals(ConstantsForLocationService.ACTION_STOP_LOCATION_SERVICE)) {
+                     stopLocationService();
+                 }
+             }
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
 }
+
+
+
 
 
 
--- app/src/main/java/com/example/sailing_tracker/RecordFragment.java
+++ app/src/main/java/com/example/sailing_tracker/RecordFragment.java
@@ -1,10 +1,10 @@
 package com.example.sailing_tracker;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,20 +17,20 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
-
 import org.apache.commons.lang3.time.StopWatch;
+
+import static java.lang.Integer.MAX_VALUE;
+
 
 public class RecordFragment extends Fragment {
 
     StopWatch watch;
+
+    // Constants
+    private static final int PERMISSION_FINE_LOCATION_CODE = 99;
 
     private static final String TAG = "RecordFragment";
 
@@ -39,25 +39,10 @@ public class RecordFragment extends Fragment {
 
     TextView mCurrent_speedTv, mBearingTv, mElapsedTimeTv;
 
-
-
-    // Location callback
-    private LocationCallback locationCallback;
-
-
-    // Constants for update intervals
-    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = 2;
-    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5;
-    private static final int PERMISSION_FINE_LOCATION = 99;
-
     // Reference to UI element record button
     Button mButtonStartLocationUpdates, mButtonStopLocationUpdates;
 
-    // Google API for location services
-    FusedLocationProviderClient fusedLocationProviderClient;
 
-    // Location request is config file for all setting related to fusedLocationProviderClient
-    LocationRequest locationRequest;
 
     public RecordFragment(){
         // Required empty public constructor
@@ -69,7 +54,6 @@ public class RecordFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_record, container, false);
 
-
         watch = new StopWatch();
 
         // Assign values
@@ -79,190 +63,81 @@ public class RecordFragment extends Fragment {
         mBearingTv = view.findViewById(R.id.bearingTv);
         mElapsedTimeTv = view.findViewById(R.id.elapsed_timeTv);
 
-
-
-
-        // Instantiate and then set all properties of LocationRequest
-        locationRequest = new LocationRequest();
-
-        // Default frequency of location check
-        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        // Frequency of update interval when set to most frequent update
-        locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
-
-        // Setting priority of the location request.
-        // i.e setting the accuracy of the location request
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        // This event is triggered whenever the update interval is met
-        // i.e. triggered every 2000ms (2 seconds)
-        locationCallback = new LocationCallback(){
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-
-                // Save the location to a variable
-                Location location = locationResult.getLastLocation();
-                double speed = location.getSpeed();
-
-                // Handle the conversion from m/s to knots using helper method toKnots
-                toKnots((int) speed);
-                double seconds = watch.getTime()/1000;
-
-
-                // Log the long and lat values but don't display to user
-                Log.d(TAG, "Latitude: " + location.getLatitude());
-                Log.d(TAG, "Longitude: " + location.getLongitude());
-
-                // Display live data to the user
-                mCurrent_speedTv.setText("Speed: " + speedInKnots + "knots");
-                mBearingTv.setText("Bearing: " + location.getBearing() + "degrees");
-                //mElapsedTimeTv.setText("Elapsed time: " + location.getTime() +" ms");
-                mElapsedTimeTv.setText("Elapsed time: " + seconds + " seconds");
-
-
-
-
-            }
-        };
-
-
         mButtonStartLocationUpdates.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                watch.start();
-                // Turn on tracking
-                startLocationUpdates();
-
+            public void onClick(View view) {
+                Log.d(TAG, "Click of start location service");
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_SHORT).show();
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            getActivity(),
+                            new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_FINE_LOCATION_CODE
+                    );
+                }  else {
+                    startLocationService();
+                }
             }
         });
 
-        mButtonStopLocationUpdates.setOnClickListener(new View.OnClickListener(){
+        mButtonStopLocationUpdates.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                watch.stop();
-                stopLocationUpdates();
+            public void onClick(View view) {
+                Log.d(TAG, "Click of stop location service");
+                stopLocationService();
             }
         });
 
-        // Call the method to retrieve last know location of the device
-        getLastKnowLocationOfDevice();
-
-        // Return the view
-       return view;
-    } // End of onCreate()
-
-
-
-    // When the activity is destroyed, location tracking is removed
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onStop() {
-        super.onStop();
-        Toast.makeText(getActivity(), "Location no longer being tracked", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onStop: Location is no longer being tracked");
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
-    }
-
-    @SuppressLint("MissingPermission")
-    private void startLocationUpdates() {
-        // Start recording location updates
-        Toast.makeText(getActivity(), "Location is being tracked", Toast.LENGTH_SHORT).show();
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
-    }
-
-    @SuppressLint("MissingPermission")
-    private void stopLocationUpdates() {
-        // Stop recording location updates
-        Toast.makeText(getActivity(), "Location is being tracked", Toast.LENGTH_SHORT).show();
-        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        return view;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-
-        // Switch statement to permission requests
-        switch (requestCode) {
-            // If the request code is 99 (PERMISSION_FINE_LOCATION)
-            case PERMISSION_FINE_LOCATION:
-                // If the result of the request permission call is 0 (granted)
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // All permissions are granted call the method updateGPS()
-                    getLastKnowLocationOfDevice();
-                } else {
-                    // Generate toast to inform user the app will not function
-                    Toast.makeText(getActivity(), "This app requires location permission to function ", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
-
-    private void getLastKnowLocationOfDevice() {
-        // 1. Get permissions from the user to use GPS services
-        // 2. Get the current location of the user from the fusedClientProvider
-        // 3. Output the update to logcat
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-
-
-
-        // Permissions check
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Permission granted
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    // Permissions are granted. Put the values of the location
-                    updateLogAndToast(location);
-
-                }
-            });
-
-
-        } else {
-            // Permission denied
-            // Ask user to grant permissions
-
-            // Check if the build version is greater than or equal to
-            // build version 23
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
+        if(requestCode == PERMISSION_FINE_LOCATION_CODE && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startLocationService();
+            } else {
+                Toast.makeText(getActivity(), "Permissions denied", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
 
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager =
+                (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null){
+            for (ActivityManager.RunningServiceInfo service :
+                activityManager.getRunningServices(MAX_VALUE)) {
+                if (LocationService.class.getName().equals(service.service.getClass())) {
+                    if (service.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+            }
+        return false;
         }
 
 
-    }
-
-    private void updateLogAndToast(Location location) {
-
-
-        if (location != null) {
-            // Update the log with new location
-            Log.d(TAG, "Latitude: " + location.getLatitude());
-            Log.d(TAG, "Longitude: " + location.getLongitude());
-
-            mCurrent_speedTv.setText("Speed: " + speedInKnots + "knots");
-            mBearingTv.setText("Bearing: " + location.getBearing());
-            mElapsedTimeTv.setText("Elapsed time: " + location.getTime());
-
-
-
-        } else {
-            Toast.makeText(getActivity(), "Location is null", Toast.LENGTH_SHORT).show();
-            Log.e(TAG, "updateLog: Location is null ");
+    private void startLocationService(){
+        if(!isLocationServiceRunning()){
+            Intent intent = new Intent(getActivity(), LocationService.class);
+            intent.setAction(ConstantsForLocationService.ACTION_START_LOCATION_SERVICE);
+            getActivity().startService(intent);
+            Toast.makeText(getActivity(), "Location service started", Toast.LENGTH_SHORT).show();
 
         }
-
     }
+    private void stopLocationService(){
+        if(isLocationServiceRunning()){
+            Intent intent = new Intent(getActivity(), LocationService.class);
+            intent.setAction(ConstantsForLocationService.ACTION_STOP_LOCATION_SERVICE);
+            getActivity().stopService(intent);
+            Toast.makeText(getActivity(), "Location service stopped", Toast.LENGTH_SHORT).show();
 
-    private void toKnots(int speed){
-        speedInKnots = (int) (speed * 1.944);
+        }
     }
 
 
